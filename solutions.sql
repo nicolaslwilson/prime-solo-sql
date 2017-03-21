d4ed48f8fd2022e9b0a9ebe763ad96cc42547848
Nic Wilson <nicwilson@gmail.com>
2017-03-21 15:33:50 -0500

    Clean up dangling semicolon

--- solutions.sql
+++ solutions.sql
@@ -24,5 +24,4 @@ INSERT INTO "syntax_practice" ("username","city","transactions_completed","trans
 VALUES ('nic','minneapolis',0,0,3.50);
 
 --The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
-DELETE FROM "syntax_practice" WHERE ("city" = 'miami' OR "city" ='phoenix') AND "transactions_completed" < 5
-;
+DELETE FROM "syntax_practice" WHERE ("city" = 'miami' OR "city" ='phoenix') AND "transactions_completed" < 5;
