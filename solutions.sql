3c28844a5f405680bce858b4fe7087315c55a8a3
nicolaslwilson <nicwilson@gmail.com>
2017-03-21 16:04:35 -0500

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -5,7 +5,7 @@ SELECT * FROM "syntax_practice" WHERE "city" ILIKE 'Chicago';
 SELECT * FROM "syntax_practice" WHERE "username" ILIKE '%a%';
 
 --The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
-UPDATE "syntax_practice" SET "account_balance" = 10.00 WHERE "account_balance" = 0.00 AND "transaction_attempted" = 0; 
+UPDATE "syntax_practice" SET "account_balance" = 10.00 WHERE "account_balance" = 0.00 AND "transactions_attempted" = 0; 
 
 --Select all users that have attempted 9 or more transactions.
 SELECT * FROM "syntax_practice" WHERE "transactions_attempted" > 9 OR "transactions_attemped" = 9;
